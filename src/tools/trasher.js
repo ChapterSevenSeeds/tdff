a18efa9bef513395bb414471c657ff487e44a20b
Tyson Jones <ballisticmissle@gmail.com>
2021-11-28 21:32:43 -0700

    Moving to trash

--- src/tools/trasher.js
+++ src/tools/trasher.js
@@ -1,18 +1,16 @@
-import trash from 'trash';
-import { unlink } from 'original-fs';
+import { shell } from 'electron';
+import { unlinkSync } from 'original-fs';
 
 addEventListener('message', async e => {
     await Promise.all([e.data.filter(x => x.selected).map(x => new Promise(async resolve => {
-            const file = x;
+            const file = x.file;
 
             // First try to send it to the trash. Then try to delete it (if it was sent to the trash, nothing should happen).
-            await trash([file]); 
-            debugger;
-            unlink(file, err => {
-                console.log(err);
+            if (!shell.moveItemToTrash(file)) {
+                unlinkSync(file);
+            }
 
-                resolve();
-            });
+            resolve();
         })
     )]);
 
